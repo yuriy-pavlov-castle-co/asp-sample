7c218b8f186584bb52b734f74f7840a1dc4d9ad5
pavlovyuriy <yuriy.pavlov@castle.co>
2016-08-28 01:45:06 +0400

    added hello world contoller

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
@@ -19,63 +15,6 @@ namespace ConsoleApplication
                 .Build();
 
             host.Run();
-        }
-    }
-
-    public class Startup
-    {
-   
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseDeveloperExceptionPage();
-
-            app.UseMvc();
-
-            //app.Use(async (context, next) =>
-            //{
-            //    await context.Response.WriteAsync("Pre Processing");
-
-            //    await next();
-
-            //    await context.Response.WriteAsync("Post Processing");
-            //});
-
-            // Handler of last Resort
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync(
-                    "Hello World of the last resort. The Time is: " +
-                    DateTime.Now.ToString("hh:mm:ss tt"));
-
-            });
-        }
-    }
-
-    public class HelloWorldController : Controller
-    {
-        [HttpGet("api/helloworld")]
-        public object HelloWorld()
-        {
-            return new
-            {
-                message = "Hello World",
-                time = DateTime.Now
-            };
-        }
-
-        [HttpGet("helloworld")]
-        public ActionResult HelloworldMvc()
-        {
-            ViewBag.Message = "Hello world!";
-            ViewBag.Time = DateTime.Now;
-
-            return View("helloworld");
-            //return View("~/helloworld.cshtml");
         }
     }
 }
